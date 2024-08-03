aa0b90531b82d851772065fa904fc459f9cc1d40
Ero-124 <khachatryanero2002@gmail.com>
2024-08-03 18:56:08 +0400

    Added a product comparison page, but for now it is in the about-us page

--- MyShop/wwwroot/assets/js/home.js
+++ MyShop/wwwroot/assets/js/home.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         breakpoints: {
             320: {
-                slidesPerView: 1.2,
+                slidesPerView: 1.5,
                 spaceBetween: 10,
             },
             768: {
